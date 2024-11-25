5ed72a0a16bd8829a85a9d780353dbdf2999e3e0
sjoshi39-shreya <sjoshi39@charlotte.edu>
2024-11-25 12:58:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "azurerm_mssql_database" "mssqldb" {
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
-  sku_name     = "S0"
+  sku_name     = "Basic"
   enclave_type = "VBS"
 
   tags = local.tags
