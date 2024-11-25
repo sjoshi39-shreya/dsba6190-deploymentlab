edb99319bd72e166c3660453a449eb1251950fa0
sjoshi39-shreya <sjoshi39@charlotte.edu>
2024-11-25 12:55:10 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ resource "random_integer" "deployment_id_suffix" {
   max = 999
 }
 
-// Resource Group
 
+// Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   location = var.location
@@ -30,15 +30,59 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = local.tags
+}
+
+// Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 // Storage Account
+resource "azurerm_storage_account" "sto" {
+  name                = "st${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  tags = local.tags
+}
+
+
+resource "azurerm_mssql_server" "mssql" {
+  name                         = "sjoshi39-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "admin"
+  administrator_login_password = "admin1234!!"
+  tags = local.tags
+}
+
+resource "azurerm_mssql_database" "mssqldb" {
+  name         = "sjoshi39-db"
+  server_id    = azurerm_mssql_server.mssql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
 
   tags = local.tags
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
 }
