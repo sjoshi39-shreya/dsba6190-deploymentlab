50b75616f310cd15addc561a3b13c9e93181a89a
Shreya Singh <sjoshi39@charlotte.edu>
2024-11-25 14:30:37 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "random_integer" "deployment_id_suffix" {
 
 // Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  #name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  name     = "rg-dsba6190-sjoshi39-dev-eastus-163"
   location = var.location
   tags     = local.tags
 }
